e74f677a3b82a1c0b1bfb46b7a331fcb831c9ffb
Anne Ouyang <anne.ouyang@gmail.com>
2024-11-02 18:56:16 -0700

    Add json format for baseline times

--- tests/test_baseline_time.py
+++ tests/test_baseline_time.py
@@ -3,11 +3,14 @@ import numpy as np
 from src.eval import load_original_model_and_inputs, time_execution_with_cuda_event, get_timing_stats, set_seed, fetch_ref_arch_from_problem_id
 from src.utils import construct_problem_dataset_from_problem_dir
 import os
+import json
 
 device = torch.device("cuda:0")
 
 REPO_TOP_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..',))
 KERNEL_BENCH_PATH = os.path.join(REPO_TOP_PATH, "KernelBench")
+
+json_results = {}
 
 def fetch_ref_arch_from_level_problem_id(level_num, problem_id, with_name=False):
     PROBLEM_DIR = os.path.join(KERNEL_BENCH_PATH, 'level'+str(level_num))
@@ -33,11 +36,12 @@ def get_time(level_num, problem_id, num_trials=100, torch_compile=False):
         torch.cuda.synchronize(device=device)
         elapsed_times = time_execution_with_cuda_event(model, *inputs, num_trials=num_trials, verbose=False, device=device)
         runtime_stats = get_timing_stats(elapsed_times, device=device)
-        print(f"Level {level_num} Problem {problem_id} Runtime Stats: {runtime_stats}")
-        # save stats to results/baseline_time.txt
-        save_path = f"results/baseline_time{'_torch_compile' if torch_compile else ''}.txt"
-        with open(save_path, "a") as f:
-            f.write(f"Level {level_num} Problem {problem_id} Runtime Stats: {runtime_stats}\n")
+        # print(f"Level {level_num} Problem {problem_id} Runtime Stats: {runtime_stats}")
+        # # save stats to results/baseline_time.txt
+        # save_path = f"results/baseline_time{'_torch_compile' if torch_compile else ''}.txt"
+        # with open(save_path, "a") as f:
+        #     f.write(f"Level {level_num} Problem {problem_id} Runtime Stats: {runtime_stats}\n")
+        json_results[f"level{level_num}"][ref_arch_name] = runtime_stats
     except Exception as e:
         print(f"[Eval] Error in Measuring Performance: {e}")
 
@@ -50,16 +54,26 @@ if __name__ == "__main__":
 
     PROBLEM_DIR_LEVEL1 = "KernelBench/level1"
     dataset_level1 = construct_problem_dataset_from_problem_dir(PROBLEM_DIR_LEVEL1)
+    json_results["level1"] = {}
     for problem_id in range(len(dataset_level1)):
         get_time(1, problem_id, torch_compile=torch_compile)
 
     PROBLEM_DIR_LEVEL2 = "KernelBench/level2"
     dataset_level2 = construct_problem_dataset_from_problem_dir(PROBLEM_DIR_LEVEL2)
+    json_results["level2"] = {}
     for problem_id in range(len(dataset_level2)):
         get_time(2, problem_id, torch_compile=torch_compile)
 
     PROBLEM_DIR_LEVEL3 = "KernelBench/level3"
     dataset_level3 = construct_problem_dataset_from_problem_dir(PROBLEM_DIR_LEVEL3)
+    json_results["level3"] = {}
     for problem_id in range(len(dataset_level3)):
         get_time(3, problem_id, torch_compile=torch_compile)
+
+    if torch_compile:
+        save_path = f"results/baseline_time_torch_compile.json"
+    else:
+        save_path = f"results/baseline_time.json"
+    with open(save_path, "w") as f:
+        json.dump(json_results, f)
 
