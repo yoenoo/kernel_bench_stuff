a5668154ac4bb3ee71f58d19d15d1a641909e95e
Simon Guo <simonguo@stanford.edu>
2025-02-20 20:04:02 -0800

    clean up run and check to support local file

--- scripts/run_and_check.py
+++ scripts/run_and_check.py
@@ -5,61 +5,80 @@ from pydra import REQUIRED, Config
 from dataclasses import dataclass
 import os
 
-from KernelBenchInternal.src import eval as kernel_eval
-from KernelBenchInternal.src import utils as kernel_utils
-from KernelBenchInternal.scripts import generate_baseline_time
+from src import eval as kernel_eval
+from src import utils as kernel_utils
+from scripts.generate_baseline_time import measure_program_time
 
-from KernelBenchInternal.src.utils import read_file
+from src.utils import read_file
 
 """
-Run a single test and check if it compiles and runs correctly
-Either do this from a file or from a database
+Run a pair of KernelBench format (problem, solution) to check if solution is correct and compute speedup
 
-1. Option 1: Ref: Manual Path, Solution: Manual path
-2. Option 2: Ref: KernelBench Problem, Solution: Manual path
-3. Option 3: Ref + Sample Fetch from database for solution and sample
+You will need two files
+1. Reference: PyTorch reference (module Model) implementation with init and input shapes
+2. Solution: PyTorch solution (module ModelNew) with inline CUDA Code
+Please see examples in src/prompts
+
+The Reference could be either
+1. a local file: specify the path to the file
+2. a kernelbench problem: specify level and problem id
+
+Usage:
+1. PyTorch reference is a local file
+python3 scripts/run_and_check.py ref_origin=local ref_arch_src_path=src/prompts/model_ex_add.py kernel_src_path=src/prompts/model_new_ex_add.py
+
+2. PyTorch refernece is a kernelbench problem
+python3 scripts/run_and_check.py ref_origin=kernelbench level=2 problem_id=40 kernel_src_path=src/prompts/model_new_ex_add.py
+
 """
 
 torch.set_printoptions(precision=4, threshold=10)
 
 class ScriptConfig(Config):
     def __init__(self):
-        self.run_name = "run_and_check" 
-        # Input src definition
-        self.eval_option = REQUIRED
 
-        # option 1: 
+        # Problem and Solution definition
+        # Input src origin definition
+        self.ref_origin = REQUIRED # either local or kernelbench
+        # ref_origin is local, specify local file path
         self.ref_arch_src_path = ""
+        # ref_origin is kernelbench, specify level and problem id
+        self.level = ""
+        self.problem_id = ""
+        # Solution src definition
         self.kernel_src_path = ""
 
-        # option 2: 
-        self.problem_id = ""
 
         # KernelBench Eval specific
+        # number of trials to run for correctness
         self.num_correct_trials = 5
+        # number of trials to run for performance
         self.num_perf_trials = 100
+        # timeout for each trial
         self.timeout = 300
+        # verbose logging
         self.verbose = False
-
-        # just running on one device
         self.measure_performance = True
+        self.build_dir_prefix = "" # if you want to specify a custom build directory
+        self.clear_cache = False # TODO
 
-
-        self.build_dir_prefix = ""
-
-        self.only_evaluate_cached = False
-        self.clear_cache = False
-
-        self.gpu_arch = ["Ada"] # build for L40s Ada Lovelace architecture
+        # Replace with your NVIDIA GPU architecture, e.g. ["Hopper"]
+        self.gpu_arch = ["Ada"] 
 
     def __repr__(self):
         return f"ScriptConfig({self.to_dict()})"
 
 def evaluate_single_sample_src(ref_arch_src: str, kernel_src: str, configs: dict, device: torch.device) -> kernel_eval.KernelExecResult:
-    # fetch kernel code from database
+    """
+    Evaluate a single sample source code against a reference source code
+    """
 
     kernel_hash = str(hash(kernel_src))
     build_dir = os.path.join(configs["build_dir_prefix"], "test_build", kernel_hash)
+    
+    if configs["clear_cache"]: # fresh kernel build
+        print(f"[INFO] Clearing cache for build directory: {build_dir}")
+        shutil.rmtree(build_dir, ignore_errors=True)
     
     num_correct_trials = configs["num_correct_trials"]
     num_perf_trials = configs["num_perf_trials"]    
@@ -84,7 +103,7 @@ def evaluate_single_sample_src(ref_arch_src: str, kernel_src: str, configs: dict
             metadata = {"cuda_error": f"CUDA Error: {str(e)}",
                         "hardware": torch.cuda.get_device_name(device=device),
                         "device": str(device)
-                        } # for debugging
+                        }
             eval_result = kernel_eval.KernelExecResult(compiled=False, correctness=False, 
                                                 metadata=metadata)
             return eval_result
@@ -92,7 +111,7 @@ def evaluate_single_sample_src(ref_arch_src: str, kernel_src: str, configs: dict
             metadata = {"other_error": f"error: {str(e)}",
                         "hardware": torch.cuda.get_device_name(device=device),
                         "device": str(device)
-                        } # for debugging
+                        }
             eval_result = kernel_eval.KernelExecResult(compiled=False, correctness=False, 
                                                 metadata=metadata)
             return eval_result
@@ -102,35 +121,59 @@ def evaluate_single_sample_src(ref_arch_src: str, kernel_src: str, configs: dict
 def main(config: ScriptConfig):
 
     print("Running with config", config)
-    
-    '''
-    Simple check
-    '''
 
-    assert config.eval_option == 1, "Only support option 1 for now"
+    # Fetch reference and kernel code
 
-    print("Using option 1: Ref: Manual Path, Solution: Manual path")
-    assert config.ref_arch_src_path != "", "ref_arch_src_path is required"
+    assert config.ref_origin == "local" or config.ref_origin == "kernelbench", "ref_origin must be either local or kernelbench"
     assert config.kernel_src_path != "", "kernel_src_path is required"  
-    ref_arch_src = read_file(config.ref_arch_src_path)
+    
+    if config.ref_origin == "local":
+        assert config.ref_arch_src_path != "", "ref_arch_src_path is required"
+        ref_arch_src = read_file(config.ref_arch_src_path)
+    elif config.ref_origin == "kernelbench":
+        raise NotImplementedError("Kernelbench problem not implemented yet")
+        ref_arch_src = kernel_utils.get_kernelbench_problem(config.level, config.problem_id)
+    else:
+        raise ValueError("Invalid ref_origin")
+    
     kernel_src = read_file(config.kernel_src_path)
-
+    print(kernel_src)
+    # Start Evaluation
     device = torch.device("cuda:0") # default device
-
     kernel_utils.set_gpu_arch(config.gpu_arch)
 
-    kernel_eval_result = evaluate_single_sample_src(ref_arch_src, kernel_src, config.to_dict(), device)
+    # Evaluate kernel against reference code
+    kernel_eval_result = evaluate_single_sample_src(
+        ref_arch_src=ref_arch_src,
+        kernel_src=kernel_src,
+        configs=config.to_dict(),
+        device=device
+    )
+    kernel_exec_time = kernel_eval_result.runtime
 
-    baseline_result = generate_baseline_time.measure_program_time(ref_arch_name=config.run_name, ref_arch_src=ref_arch_src, device=device)
+    # Measure baseline time
+    # Default using PyTorch Eager here
+    # but you can switch to torch.compile by setting use_torch_compile=True and related backend / options
+    ref_time_result = measure_program_time(ref_arch_name="Reference Program", 
+                                                ref_arch_src=ref_arch_src, 
+                                                num_trials=config.num_perf_trials,
+                                                use_torch_compile=False,
+                                                device=device)
+    ref_exec_time = ref_time_result.get("mean", None)
 
-    print(f"Kernel eval result: {kernel_eval_result}")
-    print(f"Baseline result: {baseline_result}")
+    print("="*40)
+    print(f"[Eval] Kernel eval result: {kernel_eval_result}")
+    print("-"*40)
+    print(f"[Timing] PyTorch Reference exec time: {ref_exec_time} ms")
+    print(f"[Timing] Custom Kernel exec time: {kernel_exec_time} ms")
+    print("-"*40)
     
     if kernel_eval_result.correctness:
-        print("Speedup: ", baseline_result["mean"] / kernel_eval_result.runtime)
-
+        print(f"[Speedup] Speedup: {ref_exec_time / kernel_exec_time:.2f}x")
     else:
-        print("Kernel did not pass correctness")
+        print("[Speedup] Speedup Not Available as Kernel did not pass correctness")
+
+    print("="*40)
 
 
 if __name__ == "__main__":
