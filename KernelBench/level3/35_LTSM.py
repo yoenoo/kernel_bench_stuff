de1fca385401704f11240779a5d0ca6d4017e21b
Natalia <nataliakokoromyti@gmail.com>
2025-07-11 12:57:48 -0700

    Restore 35_LTSM.py

---
+++ KernelBench/level3/35_LTSM.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+
+class Model(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size, dropout=0.0):
+        """
+        Initialize the LSTM model.
+
+        :param input_size: The number of expected features in the input `x`
+        :param hidden_size: The number of features in the hidden state `h`
+        :param num_layers: Number of recurrent layers
+        :param output_size: The number of output features
+        :param dropout: If non-zero, introduces a Dropout layer on the outputs of each LSTM layer except the last layer, with dropout probability equal to `dropout`
+        """
+        super(Model, self).__init__()
+        # Initialize hidden state with random values
+        self.h0 = torch.randn((num_layers, batch_size, hidden_size))
+        self.c0 = torch.randn((num_layers, batch_size, hidden_size))
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout, bidirectional=False)
+        self.fc = nn.Linear(hidden_size, output_size)
+    
+    def forward(self, x):
+        """
+        Forward pass through the LSTM model.
+
+        :param x: The input tensor, shape (batch_size, sequence_length, input_size)
+        :return: The output tensor, shape (batch_size, sequence_length, output_size)
+        """
+        self.h0 = self.h0.to(x.device)
+        self.c0 = self.h0.to(x.device)
+        
+        # Forward propagate LSTM
+        out, hn = self.lstm(x, (self.h0, self.c0))  # out: tensor of shape (batch_size, seq_length, hidden_size)
+        
+        # Decode the hidden state of the last time step
+        out = self.fc(out[:, -1, :])  # out: tensor of shape (batch_size, output_size)
+        
+        return out
+
+# Test code
+batch_size = 10
+sequence_length = 512
+input_size = 128
+hidden_size = 256
+num_layers = 6
+output_size = 10
+dropout = 0.0
+
+def get_inputs():
+    return [torch.randn(batch_size, sequence_length, input_size)]
+
+def get_init_inputs():
+    return [input_size, hidden_size, num_layers, output_size, dropout]
